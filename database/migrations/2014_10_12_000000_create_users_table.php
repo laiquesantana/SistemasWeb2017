ae37fb72399ea4f6a22607fbf6dcb03ba893db7d
laique santana <laiquesantana@hotmail.com>
2017-03-30 22:45:44 -0300

    login tabela de usuarios(funcionarios), valiadcoes em portugues mas precisa configurar o ::atribute dps

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('perfil')->default('user');
+            $table->string('ativo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
